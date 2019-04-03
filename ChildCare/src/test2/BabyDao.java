29c3fe24c57ef754beee8cf9d016e59e033100da
hp <rr3190849@gmail.comgit>
2019-04-04 00:12:21 +0530

    push and commit

--- ChildCare/src/test2/BabyDao.java
+++ ChildCare/src/test2/BabyDao.java
@@ -114,4 +114,36 @@ public class BabyDao {
 		}catch(Exception e){System.out.println(e);}
 		return list;
 	}
+	public static List<Baby> getRecordsBySex(String sex,String religion){
+		List<Baby> list=new ArrayList<Baby>();
+		
+		try{
+			Connection con=getCon();
+			//System.out.println(religion);
+			
+			String religion1 = religion.substring(0, 1).toUpperCase() + religion.substring(1);
+
+
+			PreparedStatement ps=con.prepareStatement("select * from babyname where sex=?and religion=? ");
+			ps.setString(1,sex);
+			ps.setString(2,religion1);
+			
+			
+			ResultSet rs=ps.executeQuery();
+			
+			while(rs.next()){
+				
+				Baby b=new Baby();
+				b.setId(rs.getInt(1));
+				b.setName(rs.getString(2));
+				b.setMeaning(rs.getString(3));
+				b.setSex(rs.getString(4));
+				b.setReligion(rs.getString(5));
+				list.add(b);
+				
+			}
+			con.close();
+		}catch(Exception e){System.out.println(e);}
+		return list;
+	}
 }
--- ChildCare/src/test3/ViewBabyNameBySex.java
+++ ChildCare/src/test3/ViewBabyNameBySex.java
@@ -12,21 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import test1.Baby;
 import test2.BabyDao;
-@WebServlet("/ViewBabyNameByReligion")
-public class ViewBabyNameByReligion extends HttpServlet {
+@WebServlet("/ViewBabyNameBySex")
+public class ViewBabyNameBySex extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
+		String sex=request.getParameter("sex");
 		String religion=request.getParameter("religion");
 		
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>View Baby by "+religion.toUpperCase()+"</title>");
+		out.println("<title>View Baby by "+sex.toUpperCase()+"</title>");
 		out.println("<link rel='stylesheet' href='resources/bootstrap.min.css'/>");
 		out.println("</head>");
 		out.println("<body>");
 		request.getRequestDispatcher("navigation.html").include(request, response);
+		//out.print(" <a href='ViewBabyNameByReligion?religion=hindu' class='btn btn-primary' role='button'>Hindu</a> ");
+		//out.print(" <a href='ViewBabyNameByReligion?religion=muslim' class='btn btn-primary' role='button'>Muslim</a> ");
+		//out.print(" <a href='ViewBabyNameByReligion?religion=sikh' class='btn btn-primary' role='button'>Sikh</a> ");
+		//out.print(" <a href='ViewBabyNameByReligion?religion=christian' class='btn btn-primary' role='button'>Christian</a> ");
 		out.print(" <a href='ViewBabyNameBySex?sex=boy&religion=hindu' class='btn btn-primary' role='button'>HinduBabyBoy</a> ");
 		out.print(" <a href='ViewBabyNameBySex?sex=boy&religion=muslim' class='btn btn-primary' role='button'>MuslimBabyBoy</a> ");
 		out.print(" <a href='ViewBabyNameBySex?sex=boy&religion=sikh' class='btn btn-primary' role='button'>SikhBabyBoy</a> ");
@@ -34,10 +39,12 @@ public class ViewBabyNameByReligion extends HttpServlet {
 		out.print(" <a href='ViewBabyNameBySex?sex=girl&religion=hindu' class='btn btn-primary' role='button'>HinduBabyGirl</a> ");
 		out.print(" <a href='ViewBabyNameBySex?sex=girl&religion=muslim' class='btn btn-primary' role='button'>MuslimBabyGirl</a> ");
 		out.print(" <a href='ViewBabyNameBySex?sex=girl&religion=sikh' class='btn btn-primary' role='button'>SikhBabyGirl</a> ");
+		out.print(" <a href='ViewBabyNameBySex?sex=girl&religion=christian' class='btn btn-primary' role='button'>ChristianBabyGirl</a> ");
+		//out.print(" <a href='ViewBabyNameBySex?sex=girl' class='btn btn-primary' role='button'>BabyGirl</a> ");
 		request.getRequestDispatcher("atoz.html").include(request, response);
 		
-		out.println("<h1>View Baby Names by "+religion.toUpperCase()+"</h1>");
-		List<Baby> list=BabyDao.getRecordsByReligion(religion);
+		out.println("<h1>View Baby  "+sex.toUpperCase()+"</h1>");
+		List<Baby> list=BabyDao.getRecordsBySex(sex,religion);
 		
 		out.print("<table class='table table-bordered table-striped'>");
 		out.println("<tr><th>Id</th><th>Name</th><th>Meaning</th><th>Sex</th><th>Religion</th></tr>");
